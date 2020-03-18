19539aa265876cf803fe1bdf79f43798ab9218b7
PA-GAGNAIRE <43635233+PA-GAGNAIRE@users.noreply.github.com>
2020-03-18 16:12:01 +0100

    Update run_ld.sh

--- RUN_LD/run_ld.sh
+++ RUN_LD/run_ld.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-bash 
+bash ./ne25m_ss10_bp5/pipeline_ldhelmet.sh
+bash ./ne25m_ss10_bp10/pipeline_ldhelmet.sh
+bash ./ne25m_ss10_bp50/pipeline_ldhelmet.sh
+bash ./ne25m_ss20_bp5/pipeline_ldhelmet.sh
+bash ./ne25m_ss20_bp10/pipeline_ldhelmet.sh
+bash ./ne25m_ss20_bp50/pipeline_ldhelmet.sh
+bash ./ne250m_ss10_bp5/pipeline_ldhelmet.sh
+bash ./ne250m_ss10_bp5/pipeline_ldhelmet.sh
+bash ./ne250m_ss10_bp10/pipeline_ldhelmet.sh
+bash ./ne250m_ss20_bp10/pipeline_ldhelmet.sh
+bash ./ne250m_ss20_bp50/pipeline_ldhelmet.sh
+bash ./ne250m_ss20_bp50/pipeline_ldhelmet.sh
+
+echo 'Finished running all 12 conditions'
