d6eac2f7c45b0707836593010aec29cb4a4d2288
PA-GAGNAIRE <43635233+PA-GAGNAIRE@users.noreply.github.com>
2020-04-09 21:32:48 +0200

    Delete qsub_LDhelmet.sh

--- qsub_LDhelmet.sh
+++
@@ -1,8 +0,0 @@
-#$ -S /bin/bash
-#$ -N Singularity_Job_REP2
-#$ -q cemeb.q
-#$ -l h_rt=10:00:00
-#$ -cwd
-#$ -V
-module load singularity/3.1
-singularity run -B ./REPLICATE_RUN_4:/RUN -B ./RUN_LD:/home/RUN_LD -B ./TMP_OUT:/home/ldhelmet/LDhelmet_v1.10/output ../Simul_LDhelmet.sif /home/RUN_LD/run_ld_12_conditions.sh
